7241f3c836a1c7aad80512b08a49924ac261ceac
Jacques Beets <jacques.beets@gmail.com>
2024-11-10 23:16:55 +0200

    retry for speech

--- scripts/optimize_media.sh
+++ scripts/optimize_media.sh
@@ -73,7 +73,7 @@ process_file() {
     # Only process audio if codec is HEVC
     if [ "$codec" = "hevc" ]; then
         echo "Video already in HEVC format, processing audio only..."
-        ffmpeg -loglevel debug -i "$input_file" -map 0:v:0 -map 0:a:m:language:eng -metadata:s:a title="2.1 Optimized" -metadata:s:a language=eng -c:v copy -c:a ac3 -ac 2 -b:a 384k -af "volume=1.5,dynaudnorm=f=150:g=15:p=0.7,loudnorm=I=-16:TP=-1.5:LRA=11" -f mp4 -movflags +faststart "$temp_output"
+        ffmpeg -loglevel debug -i "$input_file" -map 0:v:0 -map 0:a:m:language:eng -metadata:s:a title="2.1 Optimized" -metadata:s:a language=eng -c:v copy -c:a ac3 -ac 2 -b:a 384k -af "dynaudnorm=f=150:g=15:p=0.9,loudnorm=I=-16:TP=-1.5:LRA=11,volume=1.2" -f mp4 -movflags +faststart "$temp_output"
         # ffmpeg -nostdin -y \
         #     -analyzeduration 20G -probesize 20G \
         #     -i "$input_file" \
